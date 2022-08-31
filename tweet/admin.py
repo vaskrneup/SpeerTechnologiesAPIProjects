808033a7c0095e0590e23e7d9f731bf6ec306700
Bhaskar Neupane <39903236+vaskrneup@users.noreply.github.com>
2022-08-31 00:05:03 -0400

    feat: admin panel for Tweet complete.

--- tweet/admin.py
+++ tweet/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from tweet import models
 
-# Register your models here.
+
+@admin.register(models.Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    """
+    Admin page for Tweet Model. Better styling for the fields.
+    """
+
+    @staticmethod
+    def author_username(tweet):
+        return tweet.author.username
+
+    fieldsets = (
+        ("Ownership", {
+            "fields": ("author", "tweet")
+        }),
+    )
+
+    list_display = ("author_username", "striped_tweet", "creation_datetime", "update_datetime")
+    search_fields = ("tweet", "get_author_username")
+    list_filter = ("creation_datetime", "update_datetime")
+    autocomplete_fields = ("author",)
