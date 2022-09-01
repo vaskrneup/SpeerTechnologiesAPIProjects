3e0d741fd775d057b30e517dcd97c8bafad422ed
Bhaskar Neupane <39903236+vaskrneup@users.noreply.github.com>
2022-08-31 23:38:19 -0400

    feat: added support for documentation.

--- StockMarketTracking/StockMarketTracking/settings.py
+++ StockMarketTracking/StockMarketTracking/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
 
     # Third-party packages
     "rest_framework",
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.admindocs.middleware.XViewMiddleware',
 ]
 
 ROOT_URLCONF = 'StockMarketTracking.urls'
